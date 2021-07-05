80464f2878edaae42b75dd5ba4dc6a290c5d998a
Edward <14011954+0xedward@users.noreply.github.com>
2021-07-05 17:40:17 -0400

    Update _fragmentParser to use `createContextualFragment`

--- src/sanitizer.js
+++ src/sanitizer.js
@@ -23,52 +23,13 @@ export const _sanitizeDocument = function _sanitizeDocument(config, input) {
  * @param {Node} ctx DOM node
  * @param {Object} input Document or DocumentFragment
  */
-// Q: is the purpose of _fragmentParser is to parse `input` under the context of `ctx`, so that `input` can be append to `ctx` correctly and returned?
-export const _fragmentParser = function _fragmentParser(ctx, input) {
-    // note: ctx is of type Node (element?)
-    /* Reference: https://github.com/mozilla/sanitizer-polyfill/issues/3
-    * https://html.spec.whatwg.org/#concept-frag-parse-context
-    * https://w3c.github.io/DOM-Parsing/#parsing
-    * /
-    // Q: https://github.com/mozilla/sanitizer-polyfill/issues/3 references we need to call createHTMLDocument, but I don't know why it is necessary yet. Is the intention to attach the result of the parsing `input` to the empty Document we create?
-    
- 
-    // Create a new Document node, and mark it as being an HTML document.
-    let doc = document.implementation.createHTMLDocument()
-
-    
-    // Check if quirks, no-quirks, or limited-quirks mode
-    // https://dom.spec.whatwg.org/#concept-document-quirks
-    // https://dom.spec.whatwg.org/#dom-document-compatmode
-    if (ctx.ownerDocument.compatMode === "CSS1Compat") {
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Mozilla_quirks_mode_behavior
-    }
-
-
-    // Create a new HTML parser, and associate it with the just created Document node.
-    // TODO there should be documentfragment parser somewhere to reuse here...
-    // Since input is a Document or DocumentFragment, we probably just need to traverse the existing DOM tree?
-
-    // Switching parser state by node type
-    // Q: For the items missing instructions on what state the tokenizer should be (e.g. xmp, style, etc), do we assume the tokenizer should be in data state? https://html.spec.whatwg.org/#concept-frag-parse-context
-    switch (ctx.nodeType) {
-        // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
-        case Node.ELEMENT_NODE:
-            console.log(ctx.tagName," is type ELEMENT")
-            // should be using ctx.childNodes instead of ctx.children b/c ctx.children only contains element nodes - https://developer.mozilla.org/en-US/docs/Web/API/Node/childNodes#notes
-            // DocumentFragment doesn't have `childNodes` property only `children` property - https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment#methods 
-        case Node.TEXT_NODE:
-            // Q: Is it possible to get XSS through text and comment nodes? If not, we can append the TEXT node and return - https://stackoverflow.com/questions/476821/is-a-dom-text-node-guaranteed-to-not-be-interpreted-as-html
-            console.log(ctx.tagName," is type TEXT")
-            break;
-        default:
-            console.log("Unknown node type", ctx.nodeType)
-            break;
-    }
-
-    // Q: If we accept string as `input`, we can probably replace the entire function body with following:
+export const _fragmentParser = function _fragmentParser(contextNode, input) {
+    // https://html.spec.whatwg.org/#concept-frag-parse-context
+    // https://w3c.github.io/DOM-Parsing/#parsing
+    // Q: How should we be handling exceptions here?
     let range = document.createRange();
-    range.selectNode(ctx);
+    range.selectNode(contextNode);
+    // https://w3c.github.io/DOM-Parsing/#ref-for-dom-range-createcontextualfragment-2
     let documentFragment = range.createContextualFragment(input)
     return documentFragment;
 };
