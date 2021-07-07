6d3da38b72718369083ee2925e1bb8743f15ed75
Edward <14011954+0xedward@users.noreply.github.com>
2021-07-07 01:14:35 -0400

    Clean up files for review

--- src/sanitizer.js
+++ src/sanitizer.js
@@ -8,7 +8,6 @@
 // eslint-disable-next-line no-unused-vars
 export const _sanitizeDocFragment = function _sanitizeDocFragment(config, input) {
     // TODO implement this function and remove eslint-disable comment above
-    // TODO maybe we can use https://developer.mozilla.org/en-US/docs/Web/API/NodeIterator to traverse through nodes
 };
 
 // eslint-disable-next-line no-unused-vars
@@ -18,13 +17,12 @@ export const _sanitizeDocument = function _sanitizeDocument(config, input) {
 
 /**
  * @param {Node} contextNode DOM node
- * @param {DOMString} input String containing HTML the caller wants to insert into contextNode's tree - Reference: https://w3c.github.io/DOM-Parsing/#webidl-1767075462
- * @returns {DocumentFragment} TODO add return description - Reference: https://github.com/0xedward/sanitizer-polyfill/pull/1#discussion_r663745143
+ * @param {DOMString} input String containing HTML the caller wants to insert into contextNode's tree
+ * @returns {DocumentFragment} TODO add return description - https://github.com/0xedward/sanitizer-polyfill/pull/1#discussion_r663745143
  */
 export const _fragmentParser = function _fragmentParser(contextNode, input) {
     // https://html.spec.whatwg.org/#concept-frag-parse-context
     // https://w3c.github.io/DOM-Parsing/#parsing
-    // Q: How should we be handling exceptions here?
     let range = document.createRange();
     range.selectNode(contextNode);
     // https://w3c.github.io/DOM-Parsing/#ref-for-dom-range-createcontextualfragment-2
